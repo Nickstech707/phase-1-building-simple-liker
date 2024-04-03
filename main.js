36abdd4159b51dfbce3e643677080064b9621d9a
Nicholas Muriithi <nickstech707@gmail.com>
2024-04-03 23:47:52 +0300

    Bult a simple liker app

--- main.js
+++ main.js
@@ -3,7 +3,38 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+// Selects all elements with the class "like-glyph"
+const articleHearts = document.querySelectorAll(".like-glyph");
 
+// Callback function for the like action
+const likeCallback = (event) => {
+  const heart = event.target;
+  
+  // Simulates a server call
+  mimicServerCall("bogusUrl")
+    .then(() => {
+      // Updates the heart icon based on its current state
+      if (heart.innerText === EMPTY_HEART) {
+        heart.innerText = FULL_HEART;
+        heart.className = "activated-heart";
+      } else {
+        heart.innerText = EMPTY_HEART;
+        heart.className = "";
+      }
+    })
+    .catch((error) => {
+      // Displays error message in modal if server call fails
+      const modal = document.getElementById("modal");
+      modal.className = "";
+      modal.innerText = error;
+      setTimeout(() => (modal.className = "hidden"), 3000);
+    });
+};
+
+// Adds click event listeners to all heart icons
+articleHearts.forEach((glyph) => {
+  glyph.addEventListener("click", likeCallback);
+});
 
 
 
